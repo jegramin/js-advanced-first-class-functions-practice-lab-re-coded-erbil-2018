da998e1615f795d12714f1fe8cd26563561bfc6f
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 06:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logDriversByHometown(drivers,hometown){
 }
 
 function driversByRevenue(drivers){
-  return drivers.slice().sort(function(a,b){
+  return drivers.sort(function(a,b){
     return a.revenue - b.revenue;
   })
 }
