5179295e48c641ab3731430bdcfa00d09e6b98cc
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 06:14:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 // Code your solution in this file!
-function logDriverNames(drivers){
-  for(i=0; i < drivers.length -1 ; i++){
-  console.log(drivers[i].name);
-}}
 
-function logDriversByHometown(drivers, str){
-   for(i=0; i < drivers.length -1 ; i++){
-  if (str === drivers[i].hometown){
-  console.log(drivers[i].name);
-}}}
 
-function driversByRevenue(drivers){
-  return drivers.slice().sort()
-}
 
-function driversByName(drivers){
-  return drivers.sort();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function logDriverNames(drivers){
+//   for(i=0; i < drivers.length -1 ; i++){
+//   console.log(drivers[i].name);
+// }}
+
+// function logDriversByHometown(drivers, str){
+//   for(i=0; i < drivers.length -1 ; i++){
+//   if (str === drivers[i].hometown){
+//   console.log(drivers[i].name);
+// }}}
+
+// function driversByRevenue(drivers){
+//   return drivers.slice().sort()
+// }
+
+// function driversByName(drivers){
+//   return drivers.sort();
+// }
 
 
 
