5963ebbfd8fb4a75444abed33b8b4f9158216a14
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 06:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  for(i=0; i < drivers.length -1 ; i++){
+  console.log(drivers[i].name);
+}}
+
+function logDriversByHometown(drivers, str){
+   for(i=0; i < drivers.length -1 ; i++){
+  if (str === drivers[i].hometown){
+  console.log(drivers[i].name);
+}}}
+
+function driversByRevenue(drivers){
+  return drivers.slice().sort()
+}
+
+function driversByName(drivers){
+  return drivers.sort();
+}
+
+
+
+
+
+
+
