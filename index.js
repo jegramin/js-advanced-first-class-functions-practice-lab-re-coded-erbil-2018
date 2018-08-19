51576bb25eaaa06c0f2f6e9879f61e743870387d
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 06:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function driversByName(drivers){
 function totalRevenue(drivers){
   return drivers.reduce(function(agg,el,i,arr){
     let total = 0;
-    return total += el;
+    return total += el.revenue;
   },[])
 }
 
