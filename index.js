0fcd995d399f1ecfeefa2942f8863851cdb2cc4b
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 06:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function driversByRevenue(drivers){
 
 function driversByName(drivers){
   return drivers.slice().sort(function(a,b){
-    return b.localCompare(a);
+    return b.localeCompare(a);
   })
 }
 
